fd3544e9bb0283722c81efd5b205483bb57e6734
Welton Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-06-23 16:00:28 -0300

    Adiciona arquivo script.js com exercício sobre variáveis

---
+++ bloco_4/dia_1/script.js
@@ -0,0 +1,14 @@
+const NAME = "Carolina";
+const BIRTH_CITY = "Taubaté";
+let birthYear = 1998;
+
+console.log(NAME);
+console.log(BIRTH_CITY);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+// BIRTH_CITY = 'Pindamonhangaba';
+// console.log(BIRTH_CITY);
