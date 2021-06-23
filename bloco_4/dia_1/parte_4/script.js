4e71b59b4d399c757874e71b80084fc37bd8578b
Welton Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-06-23 18:45:04 -0300

    Adiciona arquivo script.js com o exercício da parte V

---
+++ bloco_4/dia_1/parte_4/script.js
@@ -0,0 +1,9 @@
+let notaDoCandidato = 92;
+
+if (notaDoCandidato >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (notaDoCandidato < 80 && notaDoCandidato >= 60) {
+  console.log("Você está na nossa lista de espera.");
+} else {
+  console.log("Você foi reprovada(o).");
+}
