04fc983a0fc5e694350a7dd1f89cb2cd4ccc1824
Welton Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-06-25 23:05:18 -0300

    Adiciona arquivo ex03.js com gabarito do exercício.

--- exercicios/ex02.js
+++ exercicios/ex02.js
@@ -1,11 +1,11 @@
 "use strict";
 
-let n = 5;
+let n = 3;
 
 if (n > 1) {
   let line = "";
   for (let index = 1; index <= n; index += 1) {
     line = line + "* ";
-    console.log(line);
+    console.log(line)
   }
 }
