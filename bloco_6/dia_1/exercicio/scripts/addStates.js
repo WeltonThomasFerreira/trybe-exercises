79cc2f1db669bfea42612dbbf5dd6406fcc045fa
Welton Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-07-15 22:49:52 -0300

    Adiciona função de popular select input-states

---
+++ bloco_6/dia_1/exercicio/scripts/addStates.js
@@ -0,0 +1,73 @@
+const states = [
+  "Acre",
+  "Alagoas",
+  "Amapá",
+  "Amazonas",
+  "Bahia",
+  "Ceará",
+  "Distrito Federal",
+  "Espírito Santo",
+  "Goiás",
+  "Maranhão",
+  "Mato Grosso",
+  "Mato Grosso do Sul",
+  "Minas Gerais",
+  "Pará",
+  "Paraíba",
+  "Paraná",
+  "Pernambuco",
+  "Piauí",
+  "Rio de Janeiro",
+  "Rio Grande do Norte",
+  "Rio Grande do Sul",
+  "Rondônia",
+  "Roraima",
+  "Santa Catarina",
+  "São Paulo",
+  "Sergipe",
+  "Tocantins",
+];
+
+const acronyms = [
+  "AC",
+  "AL",
+  "AP",
+  "AM",
+  "BA",
+  "CE",
+  "DF",
+  "ES",
+  "GO",
+  "MA",
+  "MT",
+  "MS",
+  "MG",
+  "PA",
+  "PB",
+  "PR",
+  "PE",
+  "PI",
+  "RJ",
+  "RN",
+  "RS",
+  "RO",
+  "RR",
+  "SC",
+  "SP",
+  "SE",
+  "TO",
+];
+
+function addStates() {
+  const inpuState = document.querySelector("#input-state");
+  for (let index = 0; index < states.length; index += 1) {
+    const state = states[index];
+    const acronym = acronyms[index];
+    const option = document.createElement("option");
+    option.innerHTML = state;
+    option.value = acronym;
+    inpuState.appendChild(option);
+  }
+}
+
+addStates();
